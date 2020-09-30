ce720ca98a7caa298a74941cf3b9a052f9c9762c
CameronPennington <cameron.pennington@gmail.com>
2020-09-30 14:20:50 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require("sequelize");
+const express = require("express");
+const app = express();
+
+const sequelize = new Sequelize(
+	"postgres://postgres:postgres@localhost:5432/testdb"
+);
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+const checkDB = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Db connected");
+	} catch (error) {
+		console.log("Unable to connect to DB ", error);
+	}
+};
+checkDB();
+
+app.listen(3000, () => {
+	console.log("listening");
+});
